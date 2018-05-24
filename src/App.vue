ae3cc0d7dcfac83be08485d0f4933251a21ecd96
janix <31140708+techbusker@users.noreply.github.com>
2018-05-24 18:00:58 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar absolute dark class="transparent elevation-0">
+      <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Still Alive Cafe</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn color="" flat>About</v-btn>
+          <v-btn color="" flat>Free Delivery</v-btn>
+          <v-btn color="" flat>Process</v-btn>
+          <v-btn color="" flat>Contact</v-btn>
+          <v-btn color="" flat>Sign Up</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <app-header></app-header>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+  export default {
+    data () {
+      return {
+       
+      }
+    },
+    components: {
+      'app-header': Header
+    }
+  }
+</script>
+
+<style lang="scss">
+@import url('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css');
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+   <v-jumbotron
+    :gradient="gradient"
+    src="/static/header.jpeg"
+    dark
+    height="100vh"
+  >
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex text-xs-center>
+          <h3 class="display-3">Gradient over Image</h3>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-jumbotron>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        //      items: [
+        //   {
+        //     src: 'https://images.pexels.com/photos/691582/pexels-photo-691582.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        //   },
+        //   {
+        //     src: 'https://images.pexels.com/photos/917494/pexels-photo-917494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        //   },
+        //   {
+        //     src: 'https://images.pexels.com/photos/904373/pexels-photo-904373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        //   },
+        //   {
+        //     src: 'https://images.pexels.com/photos/675278/pexels-photo-675278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        //   }
+        // ],
+
+        gradient: 'to right, #16A085, #F4D03F'
+
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.toolbar__content > *:not(.btn):not(.menu):last-child:not(:only-child), .toolbar__extension > *:not(.btn):not(.menu):last-child:not(:only-child) {
+    margin-right: 3rem;
+}
+
+.fluid {
+    padding: 0;
+    // background-color: red;
+}
+</style>
